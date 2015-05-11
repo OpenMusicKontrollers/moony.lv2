ade58a758b2388e868a09fa422f5c2d26842824b
Hanspeter Portner <dev@open-music-kontrollers.ch>
2015-05-11 16:18:45 +0200

    add unit test for API.

--- test_moony.c
+++ test_moony.c
@@ -17,41 +17,179 @@
 
 #include <moony.h>
 
-typedef struct _Handle Handle;
+#include <Eina.h>
+#include <ext_urid.h>
 
-struct _Handle {
+#include <lauxlib.h>
+
+#define BUF_SIZE 8191
+
+typedef struct _handle_t handle_t;
+
+struct _handle_t {
 	moony_t moony;
 
-	int max_val;
+	ext_urid_t *ext_urid;
 
-	const LV2_Atom_Sequence *event_in;
-	LV2_Atom_Sequence *event_out;
-	const float *val_in [4];
-	float *val_out [4];
-
-	const LV2_Atom_Sequence *control;
-	LV2_Atom_Sequence *notify;
-	
 	LV2_Atom_Forge forge;
+	uint8_t buf [8192];
 };
+
+static int
+_test(lua_State *L)
+{
+	handle_t *handle = lua_touserdata(L, lua_upvalueindex(1));
+
+	if(!lua_isfunction(L, 1) || !lua_isfunction(L, 2))
+	{
+		fprintf(stderr, "err: expected 2 function arguments\n");
+		exit(-1);
+	}
+
+	LV2_Atom_Forge *forge = &handle->forge;
+	LV2_Atom_Forge_Frame frame;
+
+	// produce events
+	lv2_atom_forge_set_buffer(forge, handle->buf, BUF_SIZE);
+	lv2_atom_forge_sequence_head(forge, &frame, 0);
+	{
+		lua_pushvalue(L, 1); // producer
+
+		lforge_t *lforge = lua_newuserdata(L, sizeof(lforge_t));
+		lforge->forge = forge;
+		luaL_getmetatable(L, "lforge");
+		lua_setmetatable(L, -2);
+
+		if(lua_pcall(L, 1, 0, 0))
+		{
+			fprintf(stderr, "err: %s\n", lua_tostring(L, -1));
+			exit(-1);
+		}
+	}
+	lv2_atom_forge_pop(forge, &frame);
+
+	// consume events
+	{
+		lua_pushvalue(L, 2); // consumer
+
+		lseq_t *lseq = lua_newuserdata(L, sizeof(lseq_t));
+		lseq->seq = (const LV2_Atom_Sequence *)handle->buf;
+		lseq->itr = NULL;
+		luaL_getmetatable(L, "lseq");
+		lua_setmetatable(L, -2);
+			
+		if(lua_pcall(L, 1, 0, 0))
+		{
+			fprintf(stderr, "err: %s\n", lua_tostring(L, -1));
+			exit(-1);
+		}
+	}
+
+	return 0;
+}
+
+static LV2_URID
+_map(LV2_URID_Map_Handle instance, const char *uri)
+{
+	handle_t *handle = instance;
+
+	return ext_urid_map(handle->ext_urid, uri);
+}
+
+static const char *
+_unmap(LV2_URID_Unmap_Handle instance, LV2_URID urid)
+{
+	handle_t *handle = instance;
+
+	return ext_urid_unmap(handle->ext_urid, urid);
+}
+
+static LV2_Worker_Status
+_sched(LV2_Worker_Schedule_Handle instance, uint32_t size, const void *data)
+{
+	handle_t *handle = instance;
+
+	printf("_sched %u\n", size);
+	//TODO
+
+	return LV2_WORKER_SUCCESS;
+}
 
 int
 main(int argc, char **argv)
 {
-	Handle *producer = calloc(1, sizeof(Handle));
-	Handle *consumer = calloc(1, sizeof(Handle));
+	static handle_t handle;
+	
+	eina_init();
 
-	if(!producer || !consumer)
+	handle.ext_urid = ext_urid_new();
+
+	LV2_URID_Map map = {
+		.handle = &handle,
+		.map = _map
+	};
+	LV2_URID_Unmap unmap = {
+		.handle = &handle,
+		.unmap = _unmap
+	};
+	LV2_Worker_Schedule sched = {
+		.handle = &handle,
+		.schedule_work = _sched
+	};
+
+	const LV2_Feature feat_map = {
+		.URI = LV2_URID__map,
+		.data = &map
+	};
+	const LV2_Feature feat_unmap = {
+		.URI = LV2_URID__unmap,
+		.data = &unmap
+	};
+	const LV2_Feature feat_sched = {
+		.URI = LV2_WORKER__schedule,
+		.data = &sched
+	};
+
+	const LV2_Feature *const features [] = {
+		&feat_map,
+		&feat_unmap,
+		&feat_sched,
+		NULL
+	};
+	
+	if(moony_init(&handle.moony, features))
+		return -1;
+
+	lua_State *L = handle.moony.vm.L;
+	moony_open(&handle.moony, L);
+
+	lua_pushstring(L, "map");
+	lua_newtable(L);
+	lua_rawset(L, LUA_REGISTRYINDEX);
+	
+	lua_pushstring(L, "unmap");
+	lua_newtable(L);
+	lua_rawset(L, LUA_REGISTRYINDEX);
+
+	lv2_atom_forge_init(&handle.forge, &map);
+
+	// register test function
+	lua_pushlightuserdata(L, &handle);
+	lua_pushcclosure(L, _test, 1);
+	lua_setglobal(L, "test");
+
+	if(luaL_dofile(L, argv[1]))
 	{
-		if(producer)
-			free(producer);
-		if(consumer)
-			free(consumer);
-
+		fprintf(stderr, "err: %s\n", lua_tostring(L, -1));
 		return -1;
 	}
 
-	//TODO write test cases
+	moony_deinit(&handle.moony);
+
+	ext_urid_free(handle.ext_urid);
+	eina_shutdown();
+
+	printf("sucess\n");
 
 	return 0;
 }
---
+++ test_moony.lua
@@ -0,0 +1,368 @@
+--[[
+	Copyright (c) 2015 Hanspeter Portner (dev@open-music-kontrollers.ch)
+	
+	This is free software: you can redistribute it and/or modify
+	it under the terms of the Artistic License 2.0 as published by
+	The Perl Foundation.
+	
+	This source is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+	Artistic License 2.0 for more details.
+	
+	You should have received a copy of the Artistic License 2.0
+	along the source as a COPYING file. If not, obtain it from
+	http://www.perlfoundation.org/artistic_license_2_0.
+--]]
+
+-- map/unmap
+do
+	local uri = 'http://test.org#foo'
+
+	assert(map[uri] == map[uri])
+	assert(map(uri) == map(uri))
+
+	local urid = map[uri]
+	assert(unmap[urid] == unmap[urid])
+	assert(unmap(urid) == unmap(urid))
+end
+
+-- Int
+do
+	local function producer(forge)
+		forge:frame_time(0)
+		forge:int(0x7fffffff)
+		
+		forge:frame_time(0)
+		forge:int(0xffffffff)
+	end
+
+	local function consumer(seq)
+		assert(#seq == 2)
+		local atom = seq[1]
+		assert(atom.type == map.Int)
+		assert(#atom == 4)
+		assert(type(atom.value) == 'number')
+		assert(atom.value == 0x7fffffff)
+		
+		local atom = seq[2]
+		assert(atom.type == map.Int)
+		assert(#atom == 4)
+		assert(type(atom.value) == 'number')
+		assert(atom.value ~= 0xffffffff)
+	end
+
+	test(producer, consumer)
+end
+
+-- Long
+do
+	local function producer(forge)
+		forge:frame_time(0)
+		forge:long(0x100000000)
+	end
+
+	local function consumer(seq)
+		assert(#seq == 1)
+		local atom = seq[1]
+		assert(atom.type == map.Long)
+		assert(#atom == 8)
+		assert(type(atom.value) == 'number')
+		assert(atom.value == 0x100000000)
+	end
+
+	test(producer, consumer)
+end
+
+-- Float
+do
+	local function producer(forge)
+		forge:frame_time(0)
+		forge:float(2.0)
+	end
+
+	local function consumer(seq)
+		assert(#seq == 1)
+		local atom = seq[1]
+		assert(atom.type == map.Float)
+		assert(#atom == 4)
+		assert(type(atom.value) == 'number')
+		assert(atom.value == 2.0)
+	end
+
+	test(producer, consumer)
+end
+
+-- Double
+do
+	local function producer(forge)
+		forge:frame_time(0)
+		forge:double(0.12)
+	end
+
+	local function consumer(seq)
+		assert(#seq == 1)
+		local atom = seq[1]
+		assert(atom.type == map.Double)
+		assert(#atom == 8)
+		assert(type(atom.value) == 'number')
+		assert(atom.value == 0.12)
+	end
+
+	test(producer, consumer)
+end
+
+-- Bool
+do
+	local function producer(forge)
+		forge:frame_time(0)
+		forge:bool(true)
+
+		forge:frame_time(0)
+		forge:bool(false)
+	end
+
+	local function consumer(seq)
+		assert(#seq == 2)
+		local atom = seq[1]
+		assert(atom.type == map.Bool)
+		assert(#atom == 4)
+		assert(type(atom.value) == 'boolean')
+		assert(atom.value == true)
+		
+		local atom = seq[2]
+		assert(atom.type == map.Bool)
+		assert(#atom == 4)
+		assert(type(atom.value) == 'boolean')
+		assert(type(atom.value) == 'boolean')
+		assert(atom.value == false)
+	end
+
+	test(producer, consumer)
+end
+
+-- String
+do
+	local str = 'hello world'
+
+	local function producer(forge)
+		forge:frame_time(0)
+		forge:string(str)
+	end
+
+	local function consumer(seq)
+		assert(#seq == 1)
+		local atom = seq[1]
+		assert(atom.type == map.String)
+		assert(#atom == string.len(str) + 1)
+		assert(type(atom.value) == 'string')
+		assert(atom.value == str)
+	end
+
+	test(producer, consumer)
+end
+
+-- Literal
+do
+	local str = 'hello world'
+	local datatype = map['http://test.org#datatype']
+	local lang = map['http://test.org#lang']
+
+	local function producer(forge)
+		forge:frame_time(0)
+		forge:literal(str, datatype, lang)
+	end
+
+	local function consumer(seq)
+		assert(#seq == 1)
+		local atom = seq[1]
+		assert(atom.type == map.Literal)
+		assert(type(atom.value) == 'string')
+		assert(#atom == string.len(str) + 1)
+		assert(atom.value == str)
+		assert(atom.datatype == datatype)
+		assert(atom.lang == lang)
+	end
+
+	test(producer, consumer)
+end
+
+-- URI
+do
+	local uri = 'http://test.org'
+
+	local function producer(forge)
+		forge:frame_time(0)
+		forge:uri(uri)
+	end
+
+	local function consumer(seq)
+		assert(#seq == 1)
+		local atom = seq[1]
+		assert(atom.type == map.URI)
+		assert(#atom == string.len(uri) + 1)
+		assert(type(atom.value) == 'string')
+		assert(atom.value == uri)
+	end
+
+	test(producer, consumer)
+end
+
+-- Path
+do
+	local path = '/tmp/lua.lua'
+
+	local function producer(forge)
+		forge:frame_time(0)
+		forge:path(path)
+	end
+
+	local function consumer(seq)
+		assert(#seq == 1)
+		local atom = seq[1]
+		assert(atom.type == map.Path)
+		assert(#atom == string.len(path) + 1)
+		assert(type(atom.value) == 'string')
+		assert(atom.value == path)
+	end
+
+	test(producer, consumer)
+end
+
+-- URID
+do
+	local uri = 'http://test.org#uri'
+	local urid = map[uri]
+
+	local function producer(forge)
+		forge:frame_time(0)
+		forge:urid(urid)
+	end
+
+	local function consumer(seq)
+		assert(#seq == 1)
+		local atom = seq[1]
+		assert(atom.type == map.URID)
+		assert(#atom == 4)
+		assert(type(atom.value) == 'number')
+		assert(atom.value == urid)
+		assert(unmap[atom.value] == uri)
+	end
+
+	test(producer, consumer)
+end
+
+-- Midi
+do
+	local m = {0x90, 0x2a, 0x7f}
+
+	local function producer(forge)
+		forge:frame_time(0)
+		forge:midi(m)
+	end
+
+	local function consumer(seq)
+		assert(#seq == 1)
+		local atom = seq[1]
+		assert(atom.type == map.Midi)
+		assert(type(atom.value) == 'table')
+		assert(#atom == #m)
+		assert(atom.value[0] == nil)
+		assert(atom.value[1] == 0x90)
+		assert(atom.value[2] == 0x2a)
+		assert(atom.value[3] == 0x7f)
+		assert(atom.value[4] == nil)
+	end
+
+	test(producer, consumer)
+end
+
+-- Tuple
+do
+	local function producer(forge)
+		forge:frame_time(0)
+		local tup = forge:tuple()
+		forge:int(1)
+		forge:float(2.0)
+		forge:long(3)
+		forge:double(4.0)
+		forge:pop(tup)
+	end
+
+	local function consumer(seq)
+		assert(#seq == 1)
+		local atom = seq[1]
+		assert(atom.type == map.Tuple)
+		assert(#atom == 4)
+
+		local sub = atom[1]
+		assert(sub.type == map.Int)
+		assert(sub.value == 1)
+		
+		sub = atom[2]
+		assert(sub.type == map.Float)
+		assert(sub.value == 2.0)
+		
+		sub = atom[3]
+		assert(sub.type == map.Long)
+		assert(sub.value == 3)
+		
+		sub = atom[4]
+		assert(sub.type == map.Double)
+		assert(sub.value == 4.0)
+
+		assert(atom[0] == nil)
+		assert(atom[5] == nil)
+
+		for i, sub in atom:foreach() do
+			assert(atom[i].value == sub.value)
+		end
+	end
+
+	test(producer, consumer)
+end
+
+-- Object
+do
+	local id = 0
+	local otype = map['http://test.org#type']
+	local key1 = map['http://test.org#key1']
+	local context2 = map['http://test.org#context2']
+	local key2 = map['http://test.org#key2']
+
+	local function producer(forge)
+		forge:frame_time(0)
+		local obj = forge:object(id, otype)
+
+		forge:key(key1)
+		forge:int(12)
+		
+		forge:property(key2, context2)
+		forge:long(13)
+	end
+
+	local function consumer(seq)
+		assert(#seq == 1)
+		local atom = seq[1]
+		assert(atom.type == map.Object)
+		assert(atom.id == 0)
+		assert(atom.otype == otype)
+		assert(#atom == 2)
+
+		local sub = atom[key1]
+		assert(sub.type == map.Int)
+		
+		sub = atom[key2]
+		assert(sub.value == 13)
+
+		for key, context, sub in atom:foreach() do
+			assert(atom[key].value == sub.value)
+
+			if(key == key2) then
+				assert(context == context2)
+			end
+		end
+	end
+
+	test(producer, consumer)
+end
