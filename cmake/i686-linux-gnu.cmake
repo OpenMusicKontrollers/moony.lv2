f38fc5b0f89a764340a4dc10e081d0293e75a4d7
Hanspeter Portner <dev@open-music-kontrollers.ch>
2016-12-06 14:31:56 +0100

    fix gitlab ci dependencies.

--- cmake/i686-linux-gnu.cmake
+++ cmake/i686-linux-gnu.cmake
@@ -5,3 +5,8 @@ set(TOOLCHAIN "i686-linux-gnu")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32" CACHE STRING "c++ flags")
 set(CMAKE_C_FLAGS   "${CMAKE_C_FLAGS} -m32" CACHE STRING "c flags")
+
+set(STATIC_SERD "/opt/${TOOLCHAIN}/lib/libserd-0.a")
+set(STATIC_SORD "/opt/${TOOLCHAIN}/lib/libsord-0.a")
+set(STATIC_CAIRO "/opt/${TOOLCHAIN}/lib/libcairo.a")
+set(STATIC_PIXMAN "/opt/${TOOLCHAIN}/lib/libpixman-1.a")
--- cmake/x86_64-linux-gnu.cmake
+++ cmake/x86_64-linux-gnu.cmake
@@ -2,3 +2,8 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR "x86_64")
 set(TOOLCHAIN "x86_64-linux-gnu")
+
+set(STATIC_SERD "/opt/${TOOLCHAIN}/lib/libserd-0.a")
+set(STATIC_SORD "/opt/${TOOLCHAIN}/lib/libsord-0.a")
+set(STATIC_CAIRO "/opt/${TOOLCHAIN}/lib/libcairo.a")
+set(STATIC_PIXMAN "/opt/${TOOLCHAIN}/lib/libpixman-1.a")
