499b1cf0ac73d58438ff0db17c5fb89e0903e359
Hanspeter Portner <dev@open-music-kontrollers.ch>
2017-06-01 22:30:29 +0200

    use propert atomic type.

--- api/api.c
+++ api/api.c
@@ -72,12 +72,12 @@ static const size_t moony_sz [MOONY_UDATA_COUNT] = {
 	[MOONY_UDATA_STASH]	= sizeof(lstash_t)
 };
 
-static _Atomic(xpress_uuid_t) voice_uuid = ATOMIC_VAR_INIT(INT64_MAX / UINT16_MAX * 2LL);
+static atomic_long voice_uuid = ATOMIC_VAR_INIT(INT64_MAX / UINT16_MAX * 2LL);
 
 __realtime static int64_t
 _voice_map_new_uuid(void *handle)
 {
-	_Atomic(xpress_uuid_t) *uuid = handle;
+	atomic_long *uuid = handle;
 	return atomic_fetch_add_explicit(uuid, 1, memory_order_relaxed);
 }
 
